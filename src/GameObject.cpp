eea447788e2dde0cfa8ea689e0fd5c2020751720
Gonzalo Guzzardi <gonzaloguzzardi@gmail.com>
2015-09-29 05:08:29 -0300

    fix en la posicion de carga del goblin

--- src/GameObject.cpp
+++ src/GameObject.cpp
@@ -14,8 +14,8 @@
 void GameObject::load(int x, int y, int width, int height,int destWidth, int destHeight, int numFrames, std::string textureID)
 {
 	m_mapPosition = Vector2D(x,y);
-	m_screenPosition = Vector2D(m_mapPosition.m_x * TheGame::Instance()->TILE_WIDTH/2 - TheGame::Instance()->TILE_WIDTH/2 - width/2,
-								m_mapPosition.m_y * TheGame::Instance()->TILE_HEIGHT - TheGame::Instance()->TILE_HEIGHT/2 - height/2);
+	m_screenPosition = Vector2D(m_mapPosition.m_x * TheGame::Instance()->TILE_WIDTH/2 + TheGame::Instance()->TILE_WIDTH/2,
+								m_mapPosition.m_y * TheGame::Instance()->TILE_HEIGHT + TheGame::Instance()->TILE_HEIGHT/2);
 	m_width = width;
 	m_height = height;
 	m_destWidth = destWidth;
